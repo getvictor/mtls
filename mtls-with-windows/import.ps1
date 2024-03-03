f54773253d1a0cccdf29cbc5fde0d4a0262286b2
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2024-03-03 11:49:06 -0600

    WIP

--- mtls-with-windows/import.ps1
+++ mtls-with-windows/import.ps1
@@ -5,7 +5,4 @@ Import-Certificate -FilePath "certs\server-ca.crt" -CertStoreLocation Cert:\Loca
 # Import the client CA so that client TLS certificates can be verified
 Import-Certificate -FilePath "certs\client-ca.crt" -CertStoreLocation Cert:\LocalMachine\Root
 # Import the client TLS certificate and key
-Import-PfxCertificate -FilePath "certs\client.pfx" -CertStoreLocation Cert:\LocalMachine\Root
-
 Import-PfxCertificate -FilePath "certs\client.pfx" -CertStoreLocation Cert:\CurrentUser\My
-
